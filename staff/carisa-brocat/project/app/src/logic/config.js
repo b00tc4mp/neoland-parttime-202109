d720dcdb1d4979776e6feaf0bb9bdc7ef32845ee
Carisa Brocat <krisabc93@gmail.com>
2022-11-29 01:05:47 +0100

    api url change

--- staff/carisa-brocat/project/app/src/logic/config.js
+++ staff/carisa-brocat/project/app/src/logic/config.js
@@ -1,6 +1,6 @@
 const config = {
     // URL: 'http://localhost:8080/api/users'
-    URL: "https://rockurafro.herokuapp.com/api"
+    URL: "https://rockurafro-backend.vercel.app/api"
 }
 
 export default config
